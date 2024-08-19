51ad76b3752ed7496d8c3e1278659f46c6963411
arnau.claramunt <arnau.claramunt@estudiantat.upc.edu>
2024-08-19 17:10:12 +0200

    revert exit button

--- src/MainApp.js
+++ src/MainApp.js
@@ -17,11 +17,6 @@ const MainApp = () => {
     setShowText(false); // Hide the text when photo is confirmed
   };
 
-  const handleExit = () => {
-    // Logic to handle exit can be added here
-    console.log('Exit button clicked');
-  };
-
   return (
     <div className="main-app">
       {capturedPhoto && (
@@ -32,7 +27,6 @@ const MainApp = () => {
       )}
       {showText && <h1>Pagina principal</h1>}
       <CameraCapture onCapture={handleCapture} onConfirm={handlePhotoConfirm} />
-      <button className="exit-button" onClick={handleExit}>Exit</button>
     </div>
   );
 };
